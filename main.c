a6ba34a5cd7dcacdb9f7ec812688e285f98f952d
Vinay Patil <vinay1645.vp@gmail.com>
2024-11-24 12:01:56 +0530

    on Hold

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
 #include<ncurses.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
 
+#define ctrl(x) ((x) & 0x1f)
 int main()
 {
     initscr();
-refresh();
+    refresh();
 
+    noecho();
     printw("Hello, World!\n");
 
     //refresh();                  // Update the screen
-    char ch;
+    int ch;
     char command[1000]={0};
     int i=0;
     //refresh();
     bool quit=false;
     while(!quit){
-        printw("VShell->> ");
+        
+        printw("Shell->> ");
+        if(strcmp(command,"quit")==0){
+            break;
+        }
         printw(command);
+        ch=getch();
         
         switch(ch){
             
-            case 'q': printw("exiting");
+            case ctrl('q'): quit=true;
                         break;
             
+            case KEY_ENTER:
+                                                                                                  
+            break;
             default:
                 command[i++]=ch;
                 //printw(command);
@@ -33,7 +46,7 @@ refresh();
 
     }
     refresh();
-    printw("thank you for using V shell ");
+    printw("thank you for using V shell : command is : %s",command);
     getch(); 
     endwin();
     return 0;
